ae0209de066ea7c773ceb0640744272bc4120a59
Jelle Akkerman <jelle@jelleakkerman.nl>
2013-01-31 20:08:34 +0400

    added css and comments

--- mondriaan.js
+++ mondriaan.js
@@ -1,15 +1,16 @@
+/* Mondriaanify by Jelle Akkerman https://github.com/jellea/mondriaanify */
 var nsh      =  require('node-syntaxhighlighter')
   , fs       =  require('fs')
   , exec     =  require('child_process').exec
   , code     =  fs.readFileSync(__filename).toString()
   , style    =  'default'
-  , htmlFile =  './mondriaan.html'
+  , htmlFile =  './victory-boogie-woogie.html'
   ;
 
 var language        =  nsh.getLanguage('js')
   , highlightedCode =  nsh.highlight(refactor(code), language);
 
-
+/* This is where the shapeing happens */ 
 function refactor (data){
   data = data.
     replace(/\n/g,'').
@@ -18,11 +19,12 @@ function refactor (data){
   rowloc = 1;
 
   output = "";
-
+  /* Iterate over every character */
   for (i=0;i<data.length;i++) {
       if (rowloc == rowlength) {
           output += '\n';
           rowloc = 0;
+          /* if half way cut down rowlength */
           if (i > data.length/2){
               rowlength -= 4;
           } else {
@@ -39,14 +41,22 @@ function refactor (data){
   return (output);
 };
 
+/* Todo: hover effect */ 
 var  html = [
         '<!DOCTYPE HTML>'
       , '<html>'
       , '<head>'
-      , '   <meta http-equiv="content-type" content="text/html; charset=utf-8"/>'
-      , '   <title>Page of Self</title>'
+      , '  <meta http-equiv="content-type" content="text/html; charset=utf-8"/>'
+      , '  <title>Victory Boogie Woogie by Mondriaan & Akkerman</title>'
       , ' <link rel="stylesheet" href="./' + style + '.css" type="text/css" media="screen" charset="utf-8" />'
-      , ' <style>body{letter-spacing: -1px;}</style>'
+      , ' <style>'
+      ,'   body{letter-spacing: -1px;}'
+      , '  .gutter {display: none;}' /* fancy mondriaan colors */
+      , '  .code .plain { color: #000 !important; background-color: #d1d1d3 !important;}' /* grey */
+      , '  .code .keyword {color: #000 !important; background-color: #d50017 !important;}' /* red */
+      , '  .code .string {color: #fff !important; background-color: #1b0091 !important;}' /* blue for strings */
+      , '  .code .comments {color: #000 !important; background-color: #feee4f !important;}' /* yellow for comments */
+      , ' </style>'
       , '</head>'
       , '<body>'
       , highlightedCode
@@ -54,6 +64,7 @@ var  html = [
       , '</html'
       ].join('\n');
 
+/* todo: write code to file */
 nsh.copyStyle(style, '.');
 fs.writeFileSync(htmlFile, html);
 
